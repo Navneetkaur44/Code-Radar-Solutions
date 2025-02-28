d45968680199d7e0fa0730e822b60b3e00780293
Navneetkaur44 <navneetkaur210206@gmail.com>
2025-02-28 18:10:18 +0530

    Solution to Input Two Integers and Print Their Sum

---
+++ Input Two Integers and Print Their Sum@@6788d5012db25efec946f93d/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
+
+return 0;
+}
